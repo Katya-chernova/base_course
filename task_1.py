79ffe9d210cf02701abfd9896e836e848d9caaa9
Katya-chernova <kachernovaty2000@gmail.com>
2024-11-30 09:26:16 +0000

    лаба4

--- task_1.py
+++ task_1.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def average(a):
-    s = 0
+    b = 0
     for i in range(len(a)):
-        s += a[i]
-    return s/len(a)
+        b = b + a[i]
+    return b/len(a)
 
 a = [1, 2, 3]
 
