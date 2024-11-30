b7f53c5908ac0f9418aecc4c0b3d5727f9fb3af1
Katya-chernova <kachernovaty2000@gmail.com>
2024-11-30 09:56:14 +0000

    лаба4

--- task_4.py
+++ task_4.py
@@ -1,16 +1,18 @@
+import numpy as np
+
 def function(a, b, N):
+    x = np.linspace(a, b, N)
+    y = x**2
+    # x = (b - a) / (N - 1)
+    # y_values = []
+    # for i in range(N):
+    #     x = a + i * x
+    #     y_values.append(x**2)
+    # return y_values
+    return y
 
-  x = (b - a) / (N - 1)
-  y_values = []
-  for i in range(N):
-    x = a + i * x
-    y_values.append(x**2)
-  return y_values
 
-a = 1
-b = 5
-N = 4
-result = function(a, b, N)
+result = function(1, 5, 100)
 print(result)
 
 
