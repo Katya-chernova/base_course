64caa2d671252b3fa4fa109a7802758b1f318bf1
Katya-chernova <kachernovaty2000@gmail.com>
2025-01-18 21:25:58 +0000

    jho

---
+++ app/keyboards.py
@@ -0,0 +1,14 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                            InlineKeyboardMarkup, InlineKeyboardButton) 
+
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
+                                     [KeyboardButton(text='Корзина')],
+                                     [KeyboardButton(text='Контакты'),
+                                      KeyboardButton(text='О нас')]],
+                            resize_keyboard=True,
+                            input_field_placeholder='Выберите пункт меню...')
+
+catalog = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Футболки', callback_data='t-shirt')],
+    [InlineKeyboardButton(text='Кроссовки', callback_data='snekers')],
+    [InlineKeyboardButton(text='Кепки', callback_data='cap')]])
