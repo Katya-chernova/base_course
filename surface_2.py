d7df0eb6d5ed525a255f32f785e06886c3e1e2b4
Katya-chernova <kachernovaty2000@gmail.com>
2025-04-25 18:34:32 +0000

    r6u

---
+++ surface_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+fix, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+phi = np.linspace(0, 2*np.pi, 100)
+theta = np.linspace(0, np.pi, 100)
+R = 5
+x = R * np.outer(np.cos(phi), np.sin(theta))
+y = R * np.outer (np.sin(phi), np.sin(theta))
+z=R* np.outer(np.ones(np.size(phi)), np.cos(theta))
+
+
+ax.plot_wireframe (x,y,z, rstride=10, cstride=10)
+
+ax.scatter (5,0,0, "o", color="r")
+plt.savefig("fig_3.png")
